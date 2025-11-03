5751744b048d5d288d38b5a9358d5bf038e4b31f
mugil027 <mugil272000@gmail.com>
2025-11-03 12:56:22 +0530

    New project update3

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -56,8 +56,8 @@ const ProjectCard = ({ project, mirror = false }) => {
       }`}
       initial={{ opacity: 0, y: 80 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.3 }}
-      transition={{ duration: 0.8, ease: "easeOut" }}
+      viewport={{ once: true, amount: 0.1 }}
+      transition={{ duration: 0.2, ease: "easeOut" }}
     >
       {/* ---------- MEDIA SECTION ---------- */}
       <div className="flex flex-col items-center justify-center w-full lg:w-[50%] xl:w-[55%]">
