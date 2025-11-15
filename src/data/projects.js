c8c820d4cb159f6c83e4fb8988eec9152e029661
mugil027 <mugil272000@gmail.com>
2025-11-15 13:56:03 +0530

    new_Project1

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,10 +2,10 @@ export const projects = [
 
   {
     id: 4,
-    title: "E-Commerce Intelligence Analytics system (Fully Automated)",
+    title: "E-Commerce Analytics system (Automated)",
     description:
-      "•	Engineered a production-style ETL pipeline using Airflow to orchestrate ingestion from AWS S3 into Snowflake and automated transformation with dbt. Built Python-based data generators, Snowflake staging layers, and a curated fact model for end-to-end data warehousing. Containerized the pipeline with Docker and implemented S3 sensors, Snowflake COPY commands, and dbt workflows for reliable, fully automated daily data refresh",
-    tech: ["Python, Airflow, DAG, AWS S3, Snowflake, dbt, Docker, Power BI"],
+      "Engineered a production-style ETL pipeline using Airflow to orchestrate ingestion from AWS S3 into Snowflake and automated transformation with dbt. Built Python-based data generators, Snowflake staging layers, and a curated fact model for end-to-end data warehousing. Containerized the pipeline with Docker and implemented S3 sensors, Snowflake COPY commands, and dbt workflows for reliable, fully automated daily data refresh",
+    tech: ["Python", "Airflow", "DAG", "AWS S3", "Snowflake", "dbt", "Docker", "Power BI"],
     video: "/videos/Ecom_Intel.mp4", // 👈 path to your project demo video
     cover: "/images/Ecom_cover.png",
     github: "",
